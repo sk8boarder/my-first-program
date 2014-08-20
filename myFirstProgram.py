c9c1e9f8e3aec8b8a9c2baf4c9d317d3976ef3ee
Leo Wan <yu.ang.wan@gmail.com>
2014-08-20 00:05:00 -0700

    Add gkw's changes.

--- myFirstProgram.py
+++ myFirstProgram.py
@@ -1,10 +1,22 @@
 #! /usr/bin/env python
 
-num = 3
-print num
+import pdb
 
-# This is great!
-# Now, assign the value 88 to the variable "blanket".
+def main():
+    #pdb.set_trace()
+    num = 3
+    print "num is: " + str(num)
+    print "num is: " + "num"
 
-# blanket = ??
-# print blanket
+    num = num + 2
+    print "num is: " + str(num)
+    print "num is: " + "num"
+
+    # This is great!
+    # Now, assign the value 88 to the variable "blanket".
+
+    blanket = 88
+    print blanket
+
+if __name__ == "__main__":
+    main()
